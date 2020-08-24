0b36aa2cb74bd55809052cd949998e15d32e3e63
sadat <sadat.himel@gmail.com>
2020-08-24 09:13:04 +0600

    pass some data through props in react

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Noyoks = ['Josim', 'Depjol', 'BappaRaj']
   return (
     <div className="App">
+      <Noyok name={Noyoks[1]} age={56}></Noyok>
+      <Noyok name={Noyoks[0]}></Noyok>
+      <Noyok name={Noyoks[2]}></Noyok>
+      <Noyok name="Shakib khan"></Noyok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+function Noyok(props){
+  console.log(props)
+  const noyokStyle = {
+    border: '2px solid purple',
+    margin: '20px'
+  }
+  return(
+    <div style={noyokStyle}>
+      <h1> Ami Noyok - {props.name}</h1>
+  <h3> I have done 5 movies {props.age || 30} years.</h3>
+    </div>
+  )
 }
 
 export default App;
